94bcbc0a5032ea187d467c677d7813bb36d31b61
eshangin <shangin.jekon@mail.ru>
2019-01-22 19:23:53 +0700

    Fill PDF logic

--- FillablePdfExample/FillablePdfExample/Controllers/HomeController.cs
+++ FillablePdfExample/FillablePdfExample/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using FillablePdfExample.Models;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,18 +16,55 @@ namespace FillablePdfExample.Controllers
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult FillPdf(Test1FormModel model)
         {
-            ViewBag.Message = "Your application description page.";
+            using (var ms = new MemoryStream())
+            {
+                using (PdfReader pdfReader = new PdfReader(Server.MapPath("~/pdf-templates/test1.pdf")))
+                using (PdfStamper pdfStamper = new PdfStamper(pdfReader, ms))
+                {
+                    AcroFields fields = pdfStamper.AcroFields;
 
-            return View();
+                    // In case FormFlattening=True Fields become empty without this flag
+                    fields.GenerateAppearances = true;
+
+                    fields.SetField("Given Name Text Box", model.GivenName ?? string.Empty);
+                    fields.SetField("Family Name Text Box", model.FamilyName ?? string.Empty);
+                    fields.SetField("Address 1 Text Box", model.Address1 ?? string.Empty);
+                    fields.SetField("Address 2 Text Box", model.Address2 ?? string.Empty);
+                    fields.SetField("House nr Text Box", model.HouseNumber ?? string.Empty);
+                    fields.SetField("Postcode Text Box", model.Postcode ?? string.Empty);
+                    fields.SetField("City Text Box", model.City ?? string.Empty);
+                    fields.SetField("Country Combo Box", model.Country ?? string.Empty);
+                    fields.SetField("Gender List Box", model.Gender ?? string.Empty);
+                    fields.SetField("Height Formatted Field", model.Height ?? string.Empty);
+                    SetCheckboxValue(fields, "Driving License Check Box", model.DrivingLicense);
+                    SetCheckboxValue(fields, "Language 1 Check Box", model.Lang1);
+                    SetCheckboxValue(fields, "Language 2 Check Box", model.Lang2);
+                    SetCheckboxValue(fields, "Language 3 Check Box", model.Lang3);
+                    SetCheckboxValue(fields, "Language 4 Check Box", model.Lang4);
+                    SetCheckboxValue(fields, "Language 5 Check Box", model.Lang5);
+                    fields.SetField("Favourite Colour List Box", model.FavColor ?? string.Empty);
+
+                    pdfStamper.FormFlattening = true;
+                }
+
+                var cd = new System.Net.Mime.ContentDisposition
+                {
+                    FileName = "result.pdf",
+                    Inline = true,
+                };
+                Response.AddHeader("Content-Disposition", cd.ToString());
+                return File(ms.ToArray(), "application/pdf");
+            }
         }
 
-        public ActionResult Contact()
+        private static void SetCheckboxValue(AcroFields fields, string fieldName, bool val)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var states = fields.GetAppearanceStates(fieldName);
+            fields.SetField(fieldName, val ? states.FirstOrDefault(s => s != "Off") : states.FirstOrDefault(s => s == "Off"), true);
+            //fields.SetField(fieldName, "Anemia");
         }
     }
 }
---
+++ FillablePdfExample/FillablePdfExample/Models/Test1FormModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FillablePdfExample.Models
+{
+    public class Test1FormModel
+    {
+        [Display(Name = "Given Name")]
+        public string GivenName { get; set; }
+
+        [Display(Name = "Family Name")]
+        public string FamilyName { get; set; }
+
+        [Display(Name = "Address 1")]
+        public string Address1 { get; set; }
+
+        [Display(Name = "Address 2")]
+        public string Address2 { get; set; }
+
+        [Display(Name = "House Number")]
+        public string HouseNumber { get; set; }
+
+        [Display(Name = "Postcode")]
+        public string Postcode { get; set; }
+
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Display(Name = "Height")]
+        public string Height { get; set; }        
+
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Display(Name = "Driving License")]
+        public bool DrivingLicense { get; set; }
+
+        [Display(Name = "Deutsch")]
+        public bool Lang1 { get; set; }
+
+        [Display(Name = "English")]
+        public bool Lang2 { get; set; }
+
+        [Display(Name = "Français")]
+        public bool Lang3 { get; set; }
+
+        [Display(Name = "Esperanto")]
+        public bool Lang4 { get; set; }
+
+        [Display(Name = "Latin")]
+        public bool Lang5 { get; set; }
+
+        [Display(Name = "Favourite colour")]
+        public string FavColor { get; set; }
+    }
+}
